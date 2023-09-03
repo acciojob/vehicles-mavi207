63e748daee0bab0e9f28f77da47c02f2a18663ba
Mohammad Maviya Waseem Shaikh <maviya9830@gmail.com>
2023-09-03 21:38:27 +0530

    submit

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -6,34 +6,8 @@ public class Vehicle {
     private int currentSpeed;
     private int currentDirection;
 
-    public int getCurrentDirection() {
-        return currentDirection;
-    }
-
-    public int getCurrentSpeed() {
-        return currentSpeed;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setCurrentSpeed(int currentSpeed) {
-        this.currentSpeed = currentSpeed;
-    }
-
-    public void setCurrentDirection(int currentDirection) {
-        this.currentDirection = currentDirection;
-    }
-
     public Vehicle(String name) {
-        this.name=name;
-        this.currentSpeed=0;
-        this.currentDirection=0;
+
     }
 
     public void steer(int direction){
@@ -41,17 +15,10 @@ public class Vehicle {
         System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
     }
 
-//    public void move(int speed, int direction){
-//        //set the values of currentSpeed and currentDirection
-//        System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
-//    }
-
-    public void move(int speed, int direction) {
-        setCurrentSpeed(speed);
-        setCurrentDirection(direction);
-        System.out.println("move method called - The speed is changed to: " + getCurrentSpeed() + ", and the direction is changed to: " + getCurrentDirection() + " degrees");
+    public void move(int speed, int direction){
+        //set the values of currentSpeed and currentDirection
+        System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
     }
-
 
     public void stop(){
 
